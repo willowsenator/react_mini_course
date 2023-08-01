0d087860db823bbb9ea8a13dd913aeeb1c4dce74
willowsenator <willowsenator@gmail.com>
2023-08-01 23:19:24 +0200

    add server and web

---
+++ web/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Home} from './components/Home'
+import { Product } from './components/Product'
+import {QueryClient, QueryClientProvider} from 'react-query'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home></Home>}> 
+              <Route path='/products' element={<Product></Product>}></Route>
+              <Route path='/eth_balance' element={<h1>ETH Balance</h1>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
